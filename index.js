5bb1e2f6e2c89e74fc4dc2598aac2da41f572f07
barungh <barungh@hotmail.com>
2023-02-10 16:19:26 +0530

    JS code improved 🔨

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-// document.addEventListener('click', (ev) => {
-//   if (ev.target.id === 'expand') {
-//     ev.target.parentElement.classList.toggle('open');
-//   }
-// })
+// Hare Krishna
 let i1 = document.getElementById('i1')
 let i2 = document.getElementById('i2')
 
 i1.style.display = 'block'
 
 i1.addEventListener('click', function(ev) {
-  ev.target.parentElement.classList.toggle('open')
+
+  p = '';
+
+  if (ev.target.nodeName === 'path') {
+    p = ev.target.parentElement.parentElement.parentElement;
+    console.log(p);
+  }
+
+  if (ev.target.nodeName === 'svg') {
+    p = ev.target.parentElement.parentElement;
+    console.log(p);
+  }
+
+  p.classList.toggle('open')
   i1.style.display = "none";
   i2.style.display = "block";
+
 })
 
 i2.addEventListener('click', function(ev) {
-  ev.target.parentElement.classList.toggle('open')
+  p = '';
+
+  if (ev.target.nodeName === 'path') {
+    p = ev.target.parentElement.parentElement.parentElement;
+    console.log(p);
+  }
+
+  if (ev.target.nodeName === 'svg') {
+    p = ev.target.parentElement.parentElement;
+    console.log(p);
+  }
+
+  p.classList.toggle('open')
   i2.style.display = "none";
   i1.style.display = "block";
 })
